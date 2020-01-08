bc34209c6bf267f2c8e4f62b3bd48be03d88fb6f
nomissivraj <simonpeterjarvis@gmail.com>
2020-01-08 22:12:06 +0000

    Implementing confirmation dialogue for delete

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -22,9 +22,17 @@ namespace Vidly
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                        "~/Scripts/custom/confirm.js"));
+
+            // Styles
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
+
         }
     }
 }
---
+++ Vidly/Scripts/custom/confirm.js
@@ -0,0 +1,49 @@
+﻿var sjModal = {};
+
+(function (self) {
+
+
+    self.init = function () {
+        // If we want to do any init/binds on load or something
+        $('#modal-container').addClass('modal-open');
+    };
+
+    self.confirm = function (target, msg, callback) {
+        self.init();
+
+        // Use msg to build confirmation box
+
+        var div = $('<div><div>').attr("id", "sjModal"),
+            message = $('<p></p>').text(msg);
+            cancel = $("<button></button>").text("Cancel").attr('onclick', 'sjModal.reject()'),
+            confirm = $("<button></button>").attr('onclick', 'sjModal.accept()').text("Confirm");
+        // On clicking anything that is not within #sjModal self.close()
+        div.append(message);
+        div.append(cancel);
+        div.append(confirm);
+
+        $('#modal-container').append(div);
+
+
+        // get result from box
+
+        // IF SUCCESS CALLBACK
+        callback();
+    };
+
+    self.reject = function () {
+        self.close();
+        console.log("reject");
+    };
+
+    self.accept = function () {
+        self.close();
+        console.log("accept");
+    };
+
+    self.close = function () {
+        $('#modal-container').removeClass('modal-open');
+        $('#sjModal').remove();
+    };
+
+})(sjModal);
